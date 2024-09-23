7cf1d236c909816d2543ff952e53eccbb063ce1b
HenriqueMoroziniHupp <henriquehupp@hotmail.com>
2024-09-23 01:13:50 -0300

    Show price in product card

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -10,36 +10,53 @@ const parallax = ref()
 function onImageLoad() {
     loading.value = false;
 }
+
+function formatPrice(price: any) {
+    return new Intl.NumberFormat(undefined, { style: 'currency', currency: 'BRL' }).format(price)
+}
+
+function hasPrice(product: IProduct) {
+    return product.priceSingle
+        || product.priceSmall
+        || product.priceMedium
+        || product.priceLarge
+}
 </script>
 
 <template>
     <Card class="card-product">
         <template #header>
-            <div class="image-wrapper parallax-image">
-                <img
-                    class=" select-none block m-auto rounded-borde aspect-video w-full object-cover"
-                    alt="user header"
-                    :src="product.imageUrl"
-                    draggable="false"
-                    @load="onImageLoad"
-                />
+            <div class="image-wrapper parallax-image absolute ">
+                <DeferredContent>
+                    <img
+                        class="select-none block m-auto aspect-video w-full object-cover"
+                        alt="user header"
+                        :src="product.imageUrl"
+                        draggable="false"
+                        @load="onImageLoad"
+                    />
+                </DeferredContent>
+                <p class="title text-xl">{{ product.name }} </p>
                 <Transition>
                     <div class="image-loading" v-if="loading"></div>
                 </Transition>
             </div>
         </template>
-        <template #title >{{ product.name }}</template>
-        <template #subtitle>{{ product.description }}</template>
-        <!-- <template #content>
-            <p class="m-0">
-                {{ product.description }}
-            </p>
-        </template> -->
-        <!-- <template #footer>
-            <div class="flex gap-4 mt-1">
-                <Button @click="loading = !loading" label="Cancel" severity="secondary" outlined class="w-full" />
-            </div>
-        </template> -->
+        <template #subtitle v-if="product.description">{{ product.description }}
+        </template>
+        <template #content>
+            <template v-if="hasPrice(product)">
+                <Badge v-if="product.priceSingle" :value="`Tamanho Unico: ${formatPrice(product.priceSingle)}`"></Badge>
+                <div v-else class="flex flex-wrap gap-2">
+                    <Badge v-if="product.priceSingle" :value="`Pequeno:  ${formatPrice(product.priceSingle)}`"></Badge>
+                    <Badge v-if="product.priceMedium" :value="`Medio:  ${formatPrice(product.priceMedium)}`"></Badge>
+                    <Badge v-if="product.priceLarge" :value="`Grande:  ${formatPrice(product.priceLarge)}`"></Badge>
+                </div>
+            </template>
+            <template v-else>
+                <Badge value="Consultar preço"></Badge>
+            </template>
+        </template>
     </Card>
 </template>
 
@@ -58,6 +75,26 @@ function onImageLoad() {
 .image-wrapper {
     position: relative;
     overflow: hidden;
+
+    .title {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        color: white;
+        padding: 0.6rem 1rem;
+        z-index: 1;
+    }
+}
+
+.image-wrapper::after {
+    content: "";
+    position: absolute;
+    z-index: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 8rem;
+    background-image: linear-gradient(180deg,transparent 50%,rgba(0,0,0,.6));
 }
 
 .image-loading {
