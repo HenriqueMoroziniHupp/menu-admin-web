2feee94ed241038ab97238fc2bc7305789ebcbb1
Henrique Hupp <henriquehupp@MBA.local>
2024-11-18 08:09:57 -0300

    remove menu on sidebar and delete uikit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import AppLayout from '@/layout/AppLayout.vue'
 import { useAuthStore } from '@/store/authStore'
+import { useUserStore } from '@/store/userStore'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -34,82 +35,6 @@ const router = createRouter({
                     path: 'qrcode',
                     name: 'QRCode',
                     component: () => import('@/views/pages/QRCode.vue')
-                },
-                {
-                    path: '/uikit/formlayout',
-                    name: 'formlayout',
-                    component: () => import('@/views/uikit/FormLayout.vue')
-                },
-                {
-                    path: '/uikit/input',
-                    name: 'input',
-                    component: () => import('@/views/uikit/InputDoc.vue')
-                },
-                {
-                    path: '/uikit/button',
-                    name: 'button',
-                    component: () => import('@/views/uikit/ButtonDoc.vue')
-                },
-                {
-                    path: '/uikit/table',
-                    name: 'table',
-                    component: () => import('@/views/uikit/TableDoc.vue')
-                },
-                {
-                    path: '/uikit/list',
-                    name: 'list',
-                    component: () => import('@/views/uikit/ListDoc.vue')
-                },
-                {
-                    path: '/uikit/tree',
-                    name: 'tree',
-                    component: () => import('@/views/uikit/TreeDoc.vue')
-                },
-                {
-                    path: '/uikit/panel',
-                    name: 'panel',
-                    component: () => import('@/views/uikit/PanelsDoc.vue')
-                },
-
-                {
-                    path: '/uikit/overlay',
-                    name: 'overlay',
-                    component: () => import('@/views/uikit/OverlayDoc.vue')
-                },
-                {
-                    path: '/uikit/media',
-                    name: 'media',
-                    component: () => import('@/views/uikit/MediaDoc.vue')
-                },
-                {
-                    path: '/uikit/message',
-                    name: 'message',
-                    component: () => import('@/views/uikit/MessagesDoc.vue')
-                },
-                {
-                    path: '/uikit/file',
-                    name: 'file',
-                    component: () => import('@/views/uikit/FileDoc.vue')
-                },
-                {
-                    path: '/uikit/menu',
-                    name: 'menu',
-                    component: () => import('@/views/uikit/MenuDoc.vue')
-                },
-                {
-                    path: '/uikit/charts',
-                    name: 'charts',
-                    component: () => import('@/views/uikit/ChartDoc.vue')
-                },
-                {
-                    path: '/uikit/misc',
-                    name: 'misc',
-                    component: () => import('@/views/uikit/MiscDoc.vue')
-                },
-                {
-                    path: '/uikit/timeline',
-                    name: 'timeline',
-                    component: () => import('@/views/uikit/TimelineDoc.vue')
                 },
                 {
                     path: '/pages/empty',
@@ -156,7 +81,7 @@ const router = createRouter({
         },
         {
             path: '/private',
-            meta: { requiresAuth: true },
+            meta: { requiresAuth: true, requireMaster: true },
             component: AppLayout,
             children: [
                 {
@@ -171,10 +96,13 @@ const router = createRouter({
 
 router.beforeEach((to, from) => {
     const authStore = useAuthStore()
+    const userStore = useUserStore()
 
     if (to.name === 'login' && authStore.isAuthenticated) return { path: '/' }
 
     if (to.meta.requiresAuth && !authStore.isAuthenticated && to.name !== 'login') return { name: 'login' }
+
+    if (to.meta.requireMaster && userStore.user.role !== 'superadmin') return { path: '/' }
 })
 
 export default router
